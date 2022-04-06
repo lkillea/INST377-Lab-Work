2a8c8abb4404b907517c559abb0e44cdadfdfc40
lkillea <64662431+lkillea@users.noreply.github.com>
2022-04-06 13:11:41 -0400

    x

---
+++ client/lab_7/script.js
@@ -0,0 +1,54 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+}
+function restoArrayMake(dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray);
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restoNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restoNum];
+  });
+    // console.log(listItems);
+  return listItems;
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.table(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
+  const form = document.querySelector('.page_item');
+  const submit = document.querySelector('.submit_button');
+  submit.style.display = 'none';
+
+  console.log('form submission'); // this is substituting for a "breakpoint"
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
+  if (arrayFromJson.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      console.log('form submission');
+      const restoArray = restoArrayMake(arrayFromJson);
+      createHtmlList(restoArray);
+    });
+  } // this is called "dot notation"
+  // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+  // it contains all 1,000 records we need
+}
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
